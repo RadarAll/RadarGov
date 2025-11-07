2e0243d0a754aee2925b8ba23e8351a0f8c05e11
ma-pedroza <matheusgomeszpedroza@gmail.com>
2025-11-07 18:22:58 -0300

    feat: component baseButton

---
+++ RadarGov.Web/LandingPage/src/components/BaseButton.vue
@@ -0,0 +1,24 @@
+<template>
+    <button :class="['w-full font-semibold py-4 rounded-full transition-all duration-300 shadow-lg mt-5', variantClass]">
+        <slot />
+    </button>
+</template>
+
+<script setup>
+defineProps(
+    {
+        variant: {
+            type: String,
+            default: 'primary'
+        }
+    }
+)
+
+const variantClass = {
+    primary: 'bg-gradient-to-r from-green-800 to-green-600 hover:from-green-700 hover:to-green-500 text-white hover:shadow-green-500/10',
+    secondary: '',
+    error: '',
+    success: '',
+}[variant]
+
+</script>
